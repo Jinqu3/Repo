7e9a6bda9d1a4f923b552684d10e67e2f4614d87
Sam Ilmetov <cepuhin.semen@gmail.com>
2024-05-02 02:03:49 +0400

    add struct CopySyntaxTree

--- TRPO_LAB_7.cpp
+++ TRPO_LAB_7.cpp
@@ -144,8 +144,37 @@ private:
 	std::string const name_; // имя переменной
 };
 
+//Реализован класс CopySyntaxTree
+struct CopySyntaxTree : Transformer
+{
+	Expression* transformNumber(Number const* number)
+	{
+		Expression* exp = new Number(number->value());
+		return exp;
+	}
+	Expression* transformBinaryOperation(BinaryOperation const* binop)
+	{
+		Expression* exp = new BinaryOperation((binop->left())->transform(this), binop->operation(), (binop->right())->transform(this));
+		return exp;
+	}
+	Expression* transformFunctionCall(FunctionCall const* fcall)
+	{
+		Expression* exp = new FunctionCall(fcall->name(), (fcall->arg())->transform(this));
+		return exp;
+	}
+	Expression* transformVariable(Variable const* var)
+	{
+		Expression* exp = new Variable(var->name());
+		return exp;
+	}
+	~CopySyntaxTree() { };
+};
+
+
+
 int main()
 {
+	//TASK1
 
 	Expression* e1 = new Number(1.234);
 	Expression* e2 = new Number(-1.234);
@@ -162,5 +191,17 @@ int main()
 	cout << callAbs->evaluate() << endl;
 
 
+	//TASK2
 
+	Number* n32 = new Number(32.0);
+	Number* n16 = new Number(16.0);
+	BinaryOperation* minus = new BinaryOperation(n32, BinaryOperation::MINUS, n16);
+	FunctionCall* callSqrt = new FunctionCall("sqrt", minus);
+	Variable* var = new Variable("var");
+	BinaryOperation* mult = new BinaryOperation(var, BinaryOperation::MUL, callSqrt);
+	FunctionCall* callAbs = new FunctionCall("abs", mult);
+	CopySyntaxTree CST;
+	Expression* newExpr = callAbs->transform(&CST);
+	std::cout << newExpr->print() << std::endl;
+	
 }
